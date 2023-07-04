87bf4c794afebac710cbe011c056c77bb6565fa3
jhonesis <87344413+jhonesis@users.noreply.github.com>
2023-07-04 15:30:09 +0200

    Update dominio.py

--- dominio.py
+++ dominio.py
@@ -7,34 +7,19 @@ subprocess.run(['sudo', 'apt', 'update'])
 subprocess.run(['sudo', 'apt-get', '-y', 'install', 'realmd', 'sssd', 'sssd-tools', 'samba-common', 'krb5-user', 'packagekit', 'samba-common-bin', 'samba-libs', 'adcli', 'ntp'])
 
 # Command 3: sudo realm join solvetic.com -U 'Administrador' -v
-subprocess.run(['sudo', 'realm', 'join', 'solvetic.com', '-U', 'Administrador', '-v'])
+subprocess.run(['sudo', 'realm', 'join', 'dominio', '-U', 'Administrador', '-v'])
 
 # Abrir el archivo con sudo nano
-subprocess.run(["sudo", "nano", "/etc/realmd.conf"])
-texto = "[users]
-default-home = /home/%D/%U
-default-shell = /bin/bash
-[active-directory]
-default-client = sssd
-os-name = Ubuntu Desktop Linux
-os-version = 20.04
-[service]
-automatic-install = no
-[solvetic.com]
-fully-qualified-names = no
-automatic-id-mapping = yes
-user-principal = yes
-manage-system = no"
-subprocess.run(["echo", f"'{texto}'", "|", "sudo", "tee", "-a", "/etc/realmd.conf"], shell=True)
+
+subprocess.Popen(["sudo", "nano", "/etc/realmd.conf"]).wait()
+texto = "[users]\ndefault-home = /home/%D/%U\ndefault-shell = /bin/bash\n[active-directory]\ndefault-client = sssd\nos-name = Ubuntu Server Linux\nos-version = 22.04\n[service]\nautomatic-install = no\n[DOMINIO]\nfully-qualified-names = no\nautomatic-id-mapping = yes\nuser-principal = yes\nmanage-system = no"
+with open("/etc/realmd.conf", "a") as f:
+    f.write(texto)
+
 
 # Abrir el archivo con sudo nano
-subprocess.run(["sudo", "nano", "/etc/pam.d/common-session"])
-texto2 = "session required pam_unix.so
-session optional pam_winbind.so
-session optional pam_sss.so
-session optional pam_systemd.so
-session required pam_mkhomedir.so skel=/etc/skel/ umask=0077"
-subprocess.run(["echo", f"'{texto2}'", "|", "sudo", "tee", "-a", "/etc/pam.d/common-session"], shell=True)
 
-
-
+subprocess.Popen(["sudo", "nano", "/etc/pam.d/common-session"]).wait()
+texto2 = "session required pam_unix.so\nsession optional pam_winbind.so\nsession optional pam_sss.so\nsession optional pam_systemd.so\nsession required pam_mkhomedir.so skel=/etc/skel/ umask=0077"
+with open("/etc/pam.d/common-session", "a") as f:
+    f.write(texto)
