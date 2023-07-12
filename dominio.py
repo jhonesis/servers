0a254e54db7e9fa9f81352e084d39055c101565f
jhonesis <87344413+jhonesis@users.noreply.github.com>
2023-07-12 11:12:14 +0200

    Update dominio.py

--- dominio.py
+++ dominio.py
@@ -21,6 +21,6 @@ print("*****************EDIT REALMD.CONF*********************")
 texto2 = "session required pam_unix.so\nsession optional pam_winbind.so\nsession optional pam_sss.so\nsession optional pam_systemd.so\nsession required pam_mkhomedir.so skel=/etc/skel/ umask=0077\nsession [default=1] pam_permit.so\nsession requisite pam_deny.so"
 os.system("echo '{}' | sudo tee /etc/pam.d/common-session > /dev/null".format(texto2))
 print("*****************EDIT PAM.D*********************")
-print("ahora... sudo nano /etc/hosts ... y adiciona: ip-server nom-server nom-server.dominio #ejm: 127.0.0.15 server25 server25.domain.com " )
+print("ahora... sudo nano /etc/hosts ... y adiciona: ip-server nom-server nom-server.dominio ejm: 127.0.0.15 server25 server25.domain.com " )
 print("ahora... sudo nano /etc/sssd/sssd.conf ... en [domain] adiciona: ad_gpo_access_control = permissive")
 print("ahora reboot y ok")
